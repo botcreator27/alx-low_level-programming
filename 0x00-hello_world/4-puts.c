df7d4e5c6729469fc26134ebc64081c7c5acde28
Isabella <mawutorcundo@gmail.com>
2023-10-12 05:19:45 -0700

    print with put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 /* This is my first code in c, exciting! */
 
 #include <stdio.h>
+#include <string.h>
 
 int main (void)
 {
-
+        char *str = "\"programming is like a multilingual puzzle";
+        puts(str);
+        return 0;
 }
