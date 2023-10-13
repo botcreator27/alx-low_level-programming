fa35c426d78c4d96d643f434201b3cd69e9708af
Isabella <mawutorcundo@gmail.com>
2023-10-13 09:59:17 -0700

    added fuction main comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 /* This is my first code in c, exciting! */
 
+/**
+ *int main- this is the entry point
+ *
+ *this function prints the string below
+ *
+ *return:0 if successful
+ */
+
 #include <stdio.h>
 #include <string.h>
 
