038f961d1437dd0f8b8b0882423924be5596155b
Isabella <mawutorcundo@gmail.com>
2023-10-13 10:12:06 -0700

    improved my function comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,21 @@
 /* This is my first code in c, exciting! */
 
 /**
- *int main- this is the entry point
+ *int main - this is the entry point
  *
- *this function prints the string below
+ *this function prints using the puts function
  *
- *return:0 if successful
+ *@param str - stores the variable
+ *
+ *return:0 on successful execution
  */
 
 #include <stdio.h>
 #include <string.h>
 
-int main (void)
+int main(void)
 {
         char *str = "\"Programming is like building a multilingual puzzle";
         puts(str);
-        return 0;
+	return(0);
 }
