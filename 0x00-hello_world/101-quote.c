f05b211d1d7eb2820e2c081959932bf6bce31541
Isabella <mawutorcundo@gmail.com>
2023-10-15 14:09:38 -0700

    using fputs

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/*lets have fun with putschar*/
+/**
+ *main - entry point 
+ *fputs - prints the string
+ *
+ *Return: 1 if sucessful
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+	return(1);
+}
