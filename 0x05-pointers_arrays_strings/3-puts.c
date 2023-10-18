5ad64b9fa2d6fc35ba8b097714ce336d84689dd2
Isabella <mawutorcundo@gmail.com>
2023-10-17 21:39:12 -0700

    done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+/**
+*_puts - prints a string
+*@str - str to be printed
+*
+*Return: void
+*/
+
+#include "main.h"
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] !=0; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
