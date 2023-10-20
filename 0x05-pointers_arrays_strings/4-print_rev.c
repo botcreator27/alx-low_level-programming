aca036287fc59008298830f6030cf9a11cdf859e
Isabella <mawutorcundo@gmail.com>
2023-10-20 07:48:18 -0700

    included param s

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 /**
  *print_rev - prints string in reverse
- *@s - string for printing
+ *@s: string for printing
  *
  *Return: void always
  */
@@ -12,7 +12,7 @@ void print_rev(char *s)
 	int count = 0;
 	int i;
 
-	for (i = 0; s[i] !='\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		count++;
 	for (i = count - 1; i >= 0; i--)
 		_putchar(s[i]);
