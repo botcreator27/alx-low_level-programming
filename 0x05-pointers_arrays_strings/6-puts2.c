b3cd022bc0d5868144fac45cdd90eb14473c28e0
Isabella <mawutorcundo@gmail.com>
2023-10-21 08:04:14 -0700

    print chars

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+/* prints characters */
+/**
+ *puts2 - prints every other character of a string
+ *@str: the string specified
+ *Return: void
+ */
+
+#include "main.h"
+
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
