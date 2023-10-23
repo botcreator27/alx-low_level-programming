5cd4f6fcbe4ba3ce6f234eee8e7e613e02e96322
Isabella <mawutorcundo@gmail.com>
2023-10-23 14:20:24 -0700

    done

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+/* locate a substring */
+/**
+ *_strstr - searches substring
+ *@needle: substring
+ *@haystack: string
+ *
+ *Return: 0 or null
+ */
+
+#include "main.h"
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+
+		if (*two == '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
