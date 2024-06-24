e23f8ed8043de8b08b478bd6fb96b9963488cc6c
Bot Creator <mawutorcundo@gmail.com>
2024-06-24 15:26:00 -0700

    prints positive, negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		{
-		printf("%d is positive\n", n)
+			printf("%d is positive\n", n);
 		}
 	else if (n < 0)
 		{
@@ -27,5 +27,5 @@ int main(void)
 		{
 			printf("%d is zero\n", n);
 		}
-	return(0)
+	return(0);
 }
