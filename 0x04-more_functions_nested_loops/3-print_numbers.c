186939d48d32ddab644c8a942be8646817242cf5
Bot Creator <mawutorcundo@gmail.com>
2024-06-28 14:59:45 -0700

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+	int x;
+	for (x = 0; x < 58; x++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+}
