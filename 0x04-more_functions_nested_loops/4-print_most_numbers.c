8194b38585e2284ba46ab56a6cbfa04a4a204e7b
Bot Creator <mawutorcundo@gmail.com>
2024-06-28 15:41:27 -0700

    done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *print_most_numbers - prints numbers except 2 and 4
-*@x: param
+* param
 *Return: returns nothing
 */
 void print_most_numbers(void)
@@ -10,13 +10,13 @@ void print_most_numbers(void)
 
 	for (; x <= 9; x++)
 	{
-	if ((x == 2) || (x == 4))
+	if (x == 2 || x == 4)
 	{
 	continue;
 	}
 	else
 	{
-	_putchar(x);
+	_putchar(x + '0');
 	}
 	}
 	_putchar('\n');
