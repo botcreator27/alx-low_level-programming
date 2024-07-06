55ab2442f3885f4feb73917da3d45191a26208b7
Bot Creator <mawutorcundo@gmail.com>
2024-07-06 10:57:07 -0700

    done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	putchar(x);
+	putchar('\n');
+	return (0);
+}
