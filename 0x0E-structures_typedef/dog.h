560da514e38e53625def2758c085b7569cf198bc
Bot Creator <mawutorcundo@gmail.com>
2024-07-23 16:22:24 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,5 +23,8 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/* defining dog_t as a new name for struct dog */
+typedef struct dog dog_t;
+
 #endif
 
