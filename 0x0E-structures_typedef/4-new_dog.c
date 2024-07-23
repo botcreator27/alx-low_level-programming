8a822abbd988a03810914732b4524ecf2ee6e3ea
Bot Creator <mawutorcundo@gmail.com>
2024-07-23 16:58:00 -0700

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,31 +16,34 @@ dog_t *new_dog(char *name, float age, char *owner)
 /* Allocate memory for the new dog structure */
 
 	dog_t *new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL) 
+
+	if (new_dog == NULL)
 	{
-	return NULL;
+	return (NULL);
 	}
 
 /* Allocate memory and copy the name */
 	new_dog->name = malloc(strlen(name) + 1);
-	if (new_dog->name == NULL) {
+	if (new_dog->name == NULL)
+	{
 	free(new_dog);
-	return NULL;
+	return (NULL);
 	}
 	strcpy(new_dog->name, name);
 
 /* Allocate memory and copy the owner */
 	new_dog->owner = malloc(strlen(owner) + 1);
-	if (new_dog->owner == NULL) {
+	if (new_dog->owner == NULL)
+	{
 	free(new_dog->name);
 	free(new_dog);
-	return NULL;
+	return (NULL);
 	}
 	strcpy(new_dog->owner, owner);
 
 /* Set the age */
 	new_dog->age = age;
 
-	return new_dog;
+	return (new_dog);
 }
 
