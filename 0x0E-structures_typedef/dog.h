5a7bd9d631f6b38b855619ebc981c6575b83ecf9
Bot Creator <mawutorcundo@gmail.com>
2024-07-23 12:06:53 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * dog - carries dog data
+ * @name: name
+ * @age; age
+ * @owner: dog owner
+ * 
+ * Description - dog data
+ *
+ */
+
 struct dog
 {
 	char *name;
