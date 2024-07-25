37424871dccc42b4f3bd7412998dba919c7adba1
Bot Creator <mawutorcundo@gmail.com>
2024-07-25 13:50:54 -0700

    this function searches for an int and prints the index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - seaches for an int
+ * @array: array being searched
+ * @cmp: pointer to function searching
+ * @size: number of elements in array
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+	return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+	if (cmp(array[i]) != 0)
+	{
+	return (i);
+	}
+	}
+
+	return (-1);
+}
+
