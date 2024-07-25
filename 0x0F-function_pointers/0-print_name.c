a5e25cf67d0a5bacc92f498024620163aaed1121
Bot Creator <mawutorcundo@gmail.com>
2024-07-25 10:54:14 -0700

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name to be printed
+ * @f: function of name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL || f != NULL)
+	f(name);
+}
+
