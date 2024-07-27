cc031d016f4577d9b84aee56bc63dc7a782c8cde
Bot Creator <mawutorcundo@gmail.com>
2024-07-27 05:53:54 -0700

    print string followed by new line

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_string - prints strings followed by a new line
+ * @seperator: string to be printed between strings
+ * @n: number of arguments passed
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list string;
+	char *str;
+	unsigned int i;
+
+	va_start(string, n);
+	for (i = 0; i < n; i++)
+	{
+	str = va_arg(string, char *);
+
+	if (string == NULL)
+	printf ("(nil)");
+	else
+	printf("%s", str);
+
+	if (separator != NULL && i != (n - 1))
+	printf("%s", separator);
+	}
+	printf("\n");
+	va_end(string);
+}
+
