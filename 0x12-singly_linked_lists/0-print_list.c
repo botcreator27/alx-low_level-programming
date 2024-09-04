768d7765505c59a6034dec5fffc9a61a466d1ce4
Bot Creator <mawutorcundo@gmail.com>
2024-09-04 09:35:37 -0700

    traverses and prints out the data stored in the node

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,2 +1,39 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - traverses the node and prints out the data stored
+ * @h: pointer to the node
+ * Return: the number of nodes traversed
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	while (h != NULL)
+	{
+	count++;
+	
+	if (h->str == NULL)
+	{
+	printf("[0] (nil)\n");
+	}
+	
+	else
+	{	
+	printf("[%d] %s\n", h->len, h->str);
+	}
+	h = h->next;
+	}
+	return (count);
+}
 
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,1 +1,20 @@
-in this project we will learn about linked lists - singly linked lists
+in this project we will learn about linked lists 
+- singly linked lists
+
+the structure of the node used is 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+
