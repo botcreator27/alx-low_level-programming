784f8b6319b8590fff51828320e793f049e47037
Bot Creator <mawutorcundo@gmail.com>
2024-09-23 07:51:31 -0700

    handle malloc fail

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,6 +14,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+	return (NULL);
+	}
 
 	new_node->n = n;
 	new_node->next = NULL;
