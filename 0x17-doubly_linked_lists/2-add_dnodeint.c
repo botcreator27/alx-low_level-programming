a57909ee06f3af28cccda9809daa6ec2b175d5ad
Bot Creator <mawutorcundo@gmail.com>
2024-09-23 04:58:17 -0700

    trial

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a node to the beginning of a dll
+ * @head: pointer to the node
+ * @n: data to be stored in node
+ *
+ * Return: success - address of new element , fail - NULL
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+	return (NULL);
+	}
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+
+	if (*head != NULL)
+	{
+	(*head)->prev = new;
+	}
+
+	return (new);
+}
